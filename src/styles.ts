b100ea71abb10b691bdd6249af2962cf866196db
“lichtle” <leticia.negreiros@outlook.com>
2024-02-09 20:52:12 -0300

    initial commit: general settings

---
+++ src/styles.ts
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from "styled-components";
+
+export const colors = {
+  background: "#FFE0FA",
+  button: "#D39EE3",
+  hover: "#B989C7",
+  text: "#FFF",
+};
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+		box-sizing: border-box;
+    list-style: none;
+    font-family: "Roboto Mono", monospace;
+    text-decoration: none;
+  }
+
+  .container {
+    max-width: 1024px;
+    width: 100%;
+    margin: 0 auto;
+  }
+
+  body {
+    background-color: ${colors.background};
+  }
+`;
+
+export default GlobalStyle;
