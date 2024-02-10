01457c379af3f21d709d876cd33497ca387fe675
“lichtle” <leticia.negreiros@outlook.com>
2024-02-09 21:48:24 -0300

    chore: creating a section container component

---
+++ src/components/Section/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  column-gap: 16px;
+  margin-bottom: 64px;
+  height: 50vh;
+`;
