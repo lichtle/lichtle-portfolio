09136851063e1e2fa5105b8efc6fc3ea02bc2400
“lichtle” <leticia.negreiros@outlook.com>
2024-02-13 19:04:30 -0300

    chore: updating the home component

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,27 +1,58 @@
-import { Intro, Technologies, Profile } from "./styles";
-import Section from "../Section";
+import { HomeContainer, Intro, Technologies, Profile } from "./styles";
+
+import profile from "../../assets/profile.jpeg";
+import paws from "../../assets/paws.png";
 
 const Home = () => (
-  <Section>
-    <>
-      <Intro>
-        <span>Olá! Meu nome é</span>
-        <h1>Leticia Fernandes</h1>
-        <span>Desenvolvedora Front-end apaixonada por gatinhos</span>
-        <Technologies>
-          <img src="https://placehold.co/25" alt="" />
-          <img src="https://placehold.co/25" alt="" />
-          <img src="https://placehold.co/25" alt="" />
-          <img src="https://placehold.co/25" alt="" />
-          <img src="https://placehold.co/25" alt="" />
-          <img src="https://placehold.co/25" alt="" />
-        </Technologies>
-      </Intro>
-      <Profile>
-        <img src="https://placehold.co/300" alt="Foto de perfil" />
-      </Profile>
-    </>
-  </Section>
+  <HomeContainer id="home">
+    <Intro>
+      <span>Olá! Meu nome é</span>
+      <h1>Leticia Fernandes</h1>
+      <span>Desenvolvedora Front-end apaixonada por gatinhos</span>
+      <img src={paws} className="divider" />
+      <Technologies>
+        <p>Domínio:</p>
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/redux/redux-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/less/less-plain-wordmark.svg" />
+      </Technologies>
+      <Technologies>
+        <p>Intermediário:</p>
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/gulp/gulp-plain.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/grunt/grunt-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jest/jest-plain.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cypressio/cypressio-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jquery/jquery-original.svg" />
+      </Technologies>
+      <Technologies>
+        <p>Básico:</p>
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vuejs/vuejs-original.svg" />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
+        />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/ruby/ruby-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/rubymine/rubymine-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original.svg" />
+      </Technologies>
+    </Intro>
+    <Profile>
+      <img src={profile} alt="Foto de perfil" />
+    </Profile>
+  </HomeContainer>
 );
 
 export default Home;
--- src/components/Home/styles.ts
+++ src/components/Home/styles.ts
@@ -1,7 +1,15 @@
 import styled from "styled-components";
 
+import { colors } from "../../styles";
+
+export const HomeContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 32px;
+  padding: 80px 32px;
+`;
+
 export const Intro = styled.div`
-  background-color: red;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -9,27 +17,41 @@ export const Intro = styled.div`
 
   h1 {
     font-size: 40px;
+    transition: 0.3s ease-in-out;
+
+    &:hover {
+      color: ${colors.hover};
+    }
   }
 
   span {
     font-size: 18px;
   }
+
+  img.divider {
+    width: 500px;
+    padding: 48px 0;
+  }
 `;
 
 export const Technologies = styled.div`
-  background-color: yellow;
+  display: flex;
+  gap: 16px;
+
   img {
-    margin-right: 16px;
+    width: 25px;
   }
 `;
 
 export const Profile = styled.div`
-  background-color: blue;
   display: flex;
-  align-items: center;
-  justify-content: center;
+  justify-content: flex-end;
 
   img {
+    width: 300px;
+    height: 300px;
     border-radius: 50%;
+    padding: 8px;
+    border: 2px solid ${colors.text};
   }
 `;
